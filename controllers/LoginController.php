140263391221db284bb7c354ebba44fe5f994340
EdrianOrtega <inuyasha09889@gmail.com>
2023-02-02 19:02:20 -0600

    sincronizando la tabla usuario a la página html

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Usuario;
 use MVC\Router;
 
 class LoginController {
@@ -22,8 +23,17 @@ class LoginController {
         echo "Desde Recuperar"; 
     }
     public static function crear(Router $router) {
-        $router->render('auth/crear-cuenta', [
 
+        $usuario = new Usuario($_POST); 
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+            $usuario->sincronizar($_POST); 
+            
+        }
+
+        $router->render('auth/crear-cuenta', [
+            'usuario' => $usuario 
         ]); 
     }
 }
